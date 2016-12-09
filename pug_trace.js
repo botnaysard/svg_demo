3efc079903033560db1fd27fe87f0ffe0e33cc73
botnaysard <scotthayward@gmail.com>
2016-12-09 14:45:05 -0500

    initial commit

---
+++ pug_trace.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	
+	$('.logo-outline').delay(2500).fadeOut(800);
+	$('.full-face').delay(2500).fadeIn(600);
+	
+/*
+	$('.logo-outline').delay(2000).fadeOut(200, function(){
+		$('.full-face').fadeIn(200); 
+	}); */
+
+	
+});
