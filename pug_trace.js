efa55db5f988d98fdbf1e707763f7ecbce33c839
botnaysard <scotthayward@gmail.com>
2016-12-12 12:46:18 -0500

    added text and rough positioning

--- pug_trace.js
+++ pug_trace.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-	
-	$('.logo-outline').delay(2500).fadeOut(800);
-	$('.full-face').delay(2500).fadeIn(600);
-	
-/*
-	$('.logo-outline').delay(2000).fadeOut(200, function(){
-		$('.full-face').fadeIn(200); 
-	}); */
-
-	
+	$('.logo-outline').delay(1300).fadeOut(800);
+	$('.full-face').delay(1300).fadeIn(600);
+	$('.top-title, .sub-title').delay(1500).fadeIn(400);
 });
