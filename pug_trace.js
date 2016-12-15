34a01f3d2c33fe773336c223dea292284380227b
botnaysard <scotthayward@gmail.com>
2016-12-15 10:35:54 -0500

    added more animation paths

--- pug_trace.js
+++ pug_trace.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('.logo-outline').delay(1300).fadeOut(800);
-	$('.full-face').delay(1300).fadeIn(600);
-	$('.top-title, .sub-title').delay(1500).fadeIn(400);
+	$('.logo-outline').delay(1100).fadeOut(800);
+	$('.full-face').delay(1100).fadeIn(600);
+	$('.top-title, .sub-title').delay(1300).fadeIn(400);
 });
